00f2ea9eeb912165d12c7563f6a8cc5b07e94a99
JobJJCaal32 <jjuarezc27@miumg.edu.gt>
2023-04-01 09:33:25 -0600

    :rocket: nueva app case

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿// definicion de variables
+int opcion; 
+double numerone; 
+double numertwo; 
+double result; 
+string message; 
+
+//dibujar menu 
+Console.WriteLine("*** Calculadora ***");
+Console.WriteLine("1. Sumar");
+Console.WriteLine("2. Restar");
+Console.WriteLine("3. Multiplicar");
+Console.WriteLine("4. Dividir");
+Console.WriteLine("5. elevar a la potencia");
+Console.WriteLine("Seleccionar un opcion entre(1 - 5)");
+
+
